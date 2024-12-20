1a19805c74f9f5be6c958a0a30f9f2a63f382802
kochevnikuz <sam.kochevnik.uz@gmail.com>
2024-12-20 15:36:35 +0500

    20.12.2024 перенос ноутбук

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,21 +1,37 @@
 from django.contrib import admin
 from .models import Collection, Carpet, TypeCarpet
+from django.utils.safestring import mark_safe
 
 # Register your models here.
 
 class CarpetAdmin(admin.ModelAdmin):
-    list_display = ('id', 'code', 'watched', 'is_published', 'collection', 'created_at', 'update_at')
+    list_display = ('id', 'code', 'watched', 'is_published', 'collection', 'created_at', 'update_at', 'get_image')
     list_display_links = ('id', 'code',)
     list_editable = ('is_published',)
-    readonly_fields = ('watched',)
+    readonly_fields = ('watched', 'get_image')
     list_filter = ('collection', 'is_published',)
 
+    def get_image(self, obj):
+        if obj.image:
+            return mark_safe(f'<img src="{obj.photo.url}" width="80px">')
+        else:
+            return 'нет картинки'
+
+    get_image.short_description = 'Миниатюра'
+
 class CollectionAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'type', 'is_published', 'created_at', 'image')
+    list_display = ('id', 'name', 'type', 'is_published', 'created_at', 'get_image')
     list_display_links = ('id', 'name',)
     list_editable = ('is_published',)
     list_filter = ('is_published', 'type', 'created_at',)
 
+    def get_image(self, obj):
+        if obj.image:
+            return mark_safe(f'<img src="{obj.image.url}" width="50px">')
+        else:
+            return 'нет картинки'
+
+    get_image.short_description = 'Миниатюра'
 
 admin.site.register(TypeCarpet)
 admin.site.register(Collection, CollectionAdmin)
--- yec/admin.py
+++ yec/admin.py
@@ -1,12 +1,23 @@
 from django.contrib import admin
 from .models import Slide
+from django.utils.safestring import mark_safe
 
 
 # Register your models here.
 class SlideAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'is_published', 'created_at')
+    list_display = ('id', 'title', 'is_published', 'created_at', 'get_image')
     list_display_links = ('id', 'title',)
     list_editable = ('is_published',)
     list_filter = ('created_at', 'is_published',)
+    fields = ('id', 'title', 'is_published', 'created_at', 'get_image')
+    readonly_fields = ('get_image', )
+
+    def get_image(self, obj):
+        if obj.image:
+            return mark_safe(f'<img src="{obj.image.url}" width="80px">')
+        else:
+            return 'нет картинки'
+
+    get_image.short_description = 'Миниатюра'
 
 admin.site.register(Slide, SlideAdmin)
